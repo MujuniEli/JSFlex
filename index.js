c4ad64d6c2a2119c21013c907f481bad5a0dbc5d
MujuniEli <elijahmujuni@gmail.com>
2021-09-12 00:18:53 +0300

    updated form validation

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ button.addEventListener('click',()=>{
 let nme = document.myForm.name;
 let mail = document.myForm.email;
 let phoneNo = document.myForm.telephone;
+let nation = document.myForm.country;
+let sex = document.myForm.gender;
+let age = document.myForm.age;
 let letters = /^[A-Za-z]+$/;
 
 function nameValidation(nme, minLen, maxLen){
@@ -21,11 +24,11 @@ function nameValidation(nme, minLen, maxLen){
         return false;
 
     }
-    if(nme.value.match(letters)){
-        alert('Use aphabetical xters only');
-        console.log('Letters have been used');
-         return true;
-    }
+    // if(nme.value.match(letters)){
+    //     alert('Use aphabetical xters only');
+    //     console.log('Letters have been used');
+    //      return true;
+    // }
     else {
         return false;
     }
@@ -33,35 +36,65 @@ function nameValidation(nme, minLen, maxLen){
 
     
 
-// function emailValidation(mail){
-//     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//     if(mail.value.match(mailformat)){
-//         return true;
-//     }else {
-//         alert('You have entered an invalid email address');
-//     }
-// }
+function emailValidation(mail){
+    let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(mail.value.match(mailformat)){
+        return true;
+    }else {
+        alert('You have entered an invalid email address');
+    }
+}
 
-// function phoneNoValidation(phoneNo){
-//     let digits = /^[0-9]+$/;
-//     if(phoneNo.value.match(digits)){
-//         return true;
-//     }else {
-//         alert('phone number must contain numbers only');
-//         return false;
-//     }
-// }
+function phoneNoValidation(phoneNo){
+    let digits = /^[0-9]+$/;
+    if(phoneNo.value.match(digits)){
+        return true;
+    }else {
+        alert('phone number must contain numbers only');
+        return false;
+    }
+}
+
+function countryValidation(nation){
+    if(nation.value == "Default"){
+        alert('select your nationality');
+        return false;
+    }else {
+        return true;
+    }
+}
+
+function genderValidation(sex){
+    if(sex.value == "Default"){
+        alert('select your gender');
+        return false;
+    }else {
+        return true;
+    }
+}
+
+function ageValidation(age){
+    if(age.value < 21){
+        alert('You are not of legal age!!');
+        return false;
+    }else {
+        return true;
+    }
+}
 
 function formValidation(){
     
-    if(nameValidation(nme, 2, 25)){}
+    if(nameValidation(nme, 2, 25)){};
 
-    // if(emailValidation(mail)){
+    if(emailValidation(mail)){};
 
-    // }
-    // if(phoneNoValidation(phoneNo)){
+    if(phoneNoValidation(phoneNo)){};
 
-    // }
+    if(countryValidation(nation)){};
+
+    if(genderValidation(sex)){};
+
+    if(ageValidation(age)){};
 
     
 }
