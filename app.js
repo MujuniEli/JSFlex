ba174826afc3183c575a3f5e879f265f3778a1b9
MujuniEli <elijahmujuni@gmail.com>
2021-09-13 20:14:06 +0300

    add app.js edit index.js

---
+++ app.js
@@ -0,0 +1,7 @@
+const body = document.querySelector('body');
+    const button = document.querySelector('button');
+
+    button.addEventListener('click',()=>{
+    body.style.backgroundColor = '#1d98d1';
+    button.style.fontSize = "25px"
+});
--- index.js
+++ index.js
@@ -1,90 +1,119 @@
-const body = document.querySelector('body');
-const button = document.querySelector('button');
-
-button.addEventListener('click',()=>{
-    body.style.backgroundColor = '#1d98d1';
-    button.style.fontSize = "25px"
-});
-
 
 let nme = document.myForm.name;
+let nmeError = document.getElementById('nmeError');
 let mail = document.myForm.email;
+let mailError = document.getElementById('mailError');
 let phoneNo = document.myForm.telephone;
+let phoneNoError = document.getElementById('phoneNoError');
 let nation = document.myForm.country;
+let nationError = document.getElementById('nationError');
 let sex = document.myForm.gender;
+let sexError = document.getElementById('sexError');
 let age = document.myForm.age;
+let ageError = document.getElementById('ageError');
+//let input = document.querySelectorAll('input');
+//let warningMsg = document.querySelector('warning');
+let error = document.getElementById('error');
 let letters = /^[A-Za-z]+$/;
 
-function nameValidation(nme, minLen, maxLen){
-    let nameLen = nme.value.length;
-    if(nameLen < 0 || nameLen < minLen || nameLen > maxLen){
-        alert('Check name characters and length');
-        preventDefault();
-        console.log('No problem with length');
-        return false;
 
-    }
-    // if(nme.value.match(letters)){
-    //     alert('Use aphabetical xters only');
-    //     console.log('Letters have been used');
-    //      return true;
-    // }
-    else {
-        return false;
-    }
-    }
+
+    function nameValidation () {
+        let nameLen = nme.value.length;
+        if (nameLen == "") {
+            nmeError.textContent = 'Please enter a name';
+          nme.style.border = "2px solid red";
+          //warningMsg.display = "block";
+          //warningMsg.textContent = 'Please enter a name';
+          //alert("Please enter a name");
+          
+          return false;
+        }
+        if (nameLen < 2) {
+          nme.style.border = "2px solid red";
+          nmeError.textContent = "*Please enter a name longer than 2 characters";
+          
+          return false;
+        }
+        if (nameLen > 25) {
+          nme.style.border = "2px solid red";
+          //alert("Please enter a name shorter than 13");
+          nmeError.textContent = "*Please enter a name shorter than 25 characters";
+          return true;
+        } else {
+            nme.style.border = '';
+            nmeError.textContent = '';
+          return false;
+        }
+      };
+    
 
     
 
 function emailValidation(mail){
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(mail.value.match(mailformat)){
+        mail.style.border = '';
+        mailError.textContent = '';
         return true;
     }else {
-        alert('You have entered an invalid email address');
+        mail.style.border = "2px solid red";
+        mailError.textContent = '*You have entered an invalid email address';
     }
 }
 
 function phoneNoValidation(phoneNo){
-    let digits = /^[0-9]+$/;
+    let digits =  /^\d{10}$/;
     if(phoneNo.value.match(digits)){
+        phoneNo.style.border = '';
+        phoneNoError.textContent = '';
         return true;
     }else {
-        alert('phone number must contain numbers only');
+        phoneNo.style.border = "2px solid red";
+        phoneNoError.textContent = 'phone number must contain numbers only';
         return false;
     }
 }
 
 function countryValidation(nation){
     if(nation.value == "Default"){
-        alert('select your nationality');
+        nation.style.border = "2px solid red";
+        nationError.textContent = 'select your nationality';
         return false;
     }else {
+        nation.style.border = 'none';
+        nationError.textContent = '';
         return true;
     }
 }
 
 function genderValidation(sex){
     if(sex.value == "Default"){
-        alert('select your gender');
+        sex.style.border = "2px solid red";
+        sexError.textContent = 'select your gender';
         return false;
     }else {
+        sex.style.border = '';
+        sexError.textContent = '';
         return true;
     }
 }
 
 function ageValidation(age){
     if(age.value < 21){
-        alert('You are not of legal age!!');
-        return false;
-    }else {
+        age.style.border = "2px solid red";
+       ageError.textContent ='You are not of legal age!!';
         return true;
+    }else {
+        age.style.border = '';
+        ageError.textContent = '';
+        return false;
     }
 }
 
-function formValidation(){
-    
-    if(nameValidation(nme, 2, 25)){};
+function formValidation(e){
+    e.preventDefault();
+    if(nameValidation()){};
 
     if(emailValidation(mail)){};
 
@@ -101,3 +130,4 @@ function formValidation(){
 
 document.addEventListener('submit', formValidation);
 
+// /^\d{10}$/ phone number regex
